5b9530e7476300186b5e2e74cc7cf8c81cff9515
Neil Mehra <neilmehra@outlook.com>
2022-03-25 13:35:03 -0400

    pain peko

--- motionProfiling.py
+++ motionProfiling.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from math import sqrt
 
 class MotionState:
     def __init__(self, x, v, a):
@@ -17,29 +18,60 @@ class MotionState:
         return 'x: {}, v: {}, a: {}'.format(self.x, self.v, self.a)
 
 class MotionConstraints:
-    def __init__(self, v_max, a_max, d_max):
+    def __init__(self, v_max, a_max, d_max, min_cruise_time=0):
         self.v_max = v_max
         self.a_max = a_max
         self.d_max = d_max
+        self.min_cruise_time = min_cruise_time
 
     def __str__(self) -> str:
-        return 'v_max: {}, a_max: {}, d_max: {}'.format(self.v_max, self.a_max, self.d_max)
+        return 'v_max: {}, a_max: {}, d_max: {}, min_cruise_time'.format(
+            self.v_max, self.a_max, self.d_max, self.min_cruise_time)
 
 class MotionProfile:
     def __init__(self, start_state: MotionState, end_state: MotionState, constraints: MotionConstraints):
         self.profileDuration = None
-        self.dt1 = (constraints.v_max - start_state.v) / constraints.a_max
-        self.dt3 = abs((end_state.v - constraints.v_max) / constraints.d_max)
+        self.accel_time = abs((constraints.v_max - start_state.v) / constraints.a_max)
+        self.deccel_time = abs((end_state.v - constraints.v_max) / constraints.d_max)
         self.accel_state = MotionState(start_state.x, start_state.v, constraints.a_max)
-        self.deccel_state = MotionState(end_state.x, end_state.v, constraints.d_max).calculate(-self.dt3)
+        self.deccel_state = MotionState(end_state.x, end_state.v, -constraints.d_max).calculate(-self.deccel_time)
 
-        # now calculate cruise state dt
-        accel_end_state = self.accel_state.calculate(self.dt1)
+        # for math check out my blog post
+        accel_end_state = self.accel_state.calculate(self.accel_time)
         self.cruise_state = MotionState(accel_end_state.x, accel_end_state.v, 0)
         delta_x = end_state.x - start_state.x
-        self.dt2 = (delta_x - self.accel_state.integrate(self.dt1) - self.deccel_state.integrate(self.dt3)) / constraints.v_max
-        self.profileDuration = self.dt1 + self.dt2 + self.dt3
-    
+        self.cruise_time = (delta_x - self.accel_state.integrate(self.accel_time) - 
+            self.deccel_state.integrate(self.deccel_time)) / constraints.v_max
+
+        if self.cruise_time < constraints.min_cruise_time:
+            self.cruise_time = constraints.min_cruise_time
+            constraints.a_max = self.max_abs(constraints.a_max, constraints.d_max)
+            constraints.d_max = constraints.a_max
+            self.accel_time = sqrt(abs(end_state.x)/abs(constraints.a_max))
+            self.deccel_time = sqrt(abs(end_state.x)/abs(constraints.d_max))
+            new_accel_end_state = self.accel_state.calculate(self.accel_time)
+            self.deccel_state = MotionState(new_accel_end_state.x, new_accel_end_state.v, -constraints.d_max)
+
+            if constraints.min_cruise_time != 0:
+                pass
+                # accel_distance = self.accel_state.integrate(self.accel_time)
+                # constraints.v_max = (delta_x - self.accel_state.integrate(self.accel_time))
+                
+
+        self.profileDuration = self.accel_time + self.cruise_time + self.deccel_time
+        self.total_integral = self.accel_state.integrate(self.accel_time) + self.cruise_state.integrate(self.cruise_time) + self.deccel_state.integrate(self.deccel_time)
+
+    def get(self, time):
+        if time <= self.accel_time:
+            return self.accel_state.calculate(time)
+        elif time <= self.accel_time + self.cruise_time:
+            return self.cruise_state.calculate(time - self.accel_time)
+        elif time <= self.profileDuration:
+            return self.deccel_state.calculate(time - self.accel_time - self.cruise_time)
+        else:
+            return self.end_state
+
+    def max_abs(self, a, b): return max(abs(a), abs(b))
 
 def plot(profile):
     time = np.arange(0, profile.profileDuration, 0.01)
@@ -47,25 +79,24 @@ def plot(profile):
     x_values = [state.x for state in states]
     v_values = [state.v for state in states]
     a_values = [state.a for state in states]
-    print('dt1: {}\ndt2: {}\ndt3: {}'.format(profile.dt1, profile.dt2, profile.dt3))
     plt.plot(time, x_values, label='position')
     plt.plot(time, v_values, label='velocity')
     plt.plot(time, a_values, label='acceleration')
     plt.legend()
     plt.show()
     
+def main():
+    a_max = 16
+    v_max = 40
+    d_max = 16
+    target = 40
+    constraints = MotionConstraints(v_max, a_max, d_max)
+    start_state = MotionState(0, 0, 0)
+    end_state = MotionState(target, 0, 0)
+    profile = MotionProfile(start_state, end_state, constraints)
+    print('dt1: {}\ndt2: {}\ndt3: {}\nprofile duration {}\n integral: {}'.format(
+        profile.accel_time, profile.cruise_time, profile.deccel_time, profile.profileDuration, profile.total_integral))
+    plot(profile)
 
-a_max = 12
-v_max = 10
-d_max = 4
-target = 30
-constraints = MotionConstraints(v_max, a_max, d_max)
-start_state = MotionState(0, 0, 0)
-end_state = MotionState(target, 0, 0)
-profile = MotionProfile(start_state, end_state, constraints)
-print('dt1: {}\ndt2: {}\ndt3: {}\nprofile duration'.format(profile.dt1, profile.dt2, profile.dt3, profile.profileDuration))
-# print('int 1: {}\nint 2: {}\nint 3: {}'.format(
-#     profile.accel_state.integrate(profile.dt1), 
-#     profile.cruise_state.integrate(profile.dt2), 
-#     profile.deccel_state.integrate(profile.dt3
-# )))
+if __name__ == '__main__':
+    main()
